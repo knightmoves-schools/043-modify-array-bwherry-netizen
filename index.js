33e4126a4a05dc4e5841a1c386dda73cad4f5d7d
Brendon W. <bwherry@midland.k12.ia.us>
2025-11-21 12:23:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-let seasons = ['Summer', 'Autumn', 'Winter', 'Spring']
+let todos = ['ela', 'math', 'sci'];
+
+function markAsDone (todos) {
+  todos[0] = 'done-ela';
+  todos[1] = 'done-math';
+  todos[2] = 'done-sci';
+}
+
+while(todos == ['ela', 'math', 'sci']){
+  return 'not done';
+}
 
 
-
-
-document.getElementById("result").innerHTML = seasons[1];
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.getElementById("result").innerHTML = "done";
 
 
